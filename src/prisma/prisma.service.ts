386111fdcd4fc2a55b62a9044aedc3267615fee4
lesgrosman <andreyagasov@gmail.com>
2022-10-02 11:35:14 +0200

    signup

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,20 @@
+import { INestApplication, Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: 'postgresql://postgres:postgres@localhost:5432/nest?schema=public',
+        },
+      },
+    });
+  }
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
